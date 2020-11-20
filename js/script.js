ed5a815b4b2021e74c2be302c08844c7c6c05d1a
Quest McGill <jamcgill93@gmail.com>
2020-11-20 11:10:47 -0500

    Finished up CSS, add JavaScript

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var fields = {};
+
+document.addEventListener("DOMContentLoaded", function(){
+    fields.name = document.getElementById('name');
+    fields.email = document.getElementById('email');
+    fields.question = document.getElementById('question')
+})
+
+function isNotEmpty(value) {
+    if (value == null || typeof value == 'undefined' ) return false;
+    return (value.length > 0);
+}
+
+function isEmail(email) {
+    let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return regex.test(String(email).toLowerCase());
+}
+
+function fieldValidation(field, validationFunction) {
+    if (field == null) return false;
+   
+    let isFieldValid = validationFunction(field.value)
+    if (!isFieldValid) {
+        field.className = 'placeholderRed';
+    } else {
+        field.className = '';
+    }
+   
+    return isFieldValid;
+}
+
+function isValid() {
+    var valid = true;
+    
+    valid &= fieldValidation(fields.name, isNotEmpty);
+    valid &= fieldValidation(fields.email, isEmail);
+    valid &= fieldValidation(fields.question, isNotEmpty);
+   
+    return valid;
+}
+
+class User {
+    constructor(name, email, question) {
+    this.name = name;
+    this.email = email;
+    this.question = question;
+    }
+}
+
+function sendContact() {
+    if (isValid()){
+        alert(`Thanks for the email.`)
+    } else {
+        alert("There was an error.")
+    }
+}
